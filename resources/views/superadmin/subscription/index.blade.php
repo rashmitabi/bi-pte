bcaf8cf8071de1ee3d2ed87534389081e439e15c
Abhishek <abhishek@broadviewinnovations.in>
2021-06-09 15:33:48 +0530

    Subscription page design

--- resources/views/superadmin/subscription/index.blade.php
+++ resources/views/superadmin/subscription/index.blade.php
@@ -1,1 +1,26 @@
-index.blade.php
+@extends('layouts.appSuperAdmin')
+@section('content')
+<!-- Page Content  -->
+<div id="content">
+
+    <section class="top-title-button mb-3">
+        <div class="row mx-0 align-items-center">
+            <div class="col-12 col-md-12 col-xl-8 col-sm-8 left">
+                <h1 class="title mb-4">Manage Subscription</h1>
+            </div>
+            <div class="col-12 col-md-12 col-xl-4 col-sm-4 right">
+                <button type="button" class="btn btn-primary"><i class="fas fa-plus-circle mr-1"></i> New Subscription</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="top-title-button white-bg mb-3">
+        <div class="row mx-0 align-items-center">
+            <div class="col-12 col-md-12 col-xl-8 col-sm-8 left">
+                <h1 class="title mb-4">Manage Subscription</h1>
+            </div>
+        </div>
+    </section>
+
+</div>
+@endsection
