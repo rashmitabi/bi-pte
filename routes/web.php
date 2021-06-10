9f37a0a8f0271abb8974d782f5a7a657fe1e86f1
rashmitabi <71814733+rashmitabi@users.noreply.github.com>
2021-06-10 17:14:54 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -45,9 +45,7 @@ Route::group(['middleware' => ['auth', 'verified','student']], function () {
 
 
     Route::resource('superadmin/subscription', App\Http\Controllers\SuperAdmin\SubscriptionsController::class);
-
     Route::resource('superadmin/users/index', App\Http\Controllers\SuperAdmin\ManageUserController::class);
-
     Route::resource('superadmin/device', App\Http\Controllers\SuperAdmin\DeviceController::class);
 
 
