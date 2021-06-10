50ccb41d39f3295d0748eaf40e25c96c3b2fbf65
rashmitabi <71814733+rashmitabi@users.noreply.github.com>
2021-06-10 19:15:16 +0530

    update

--- resources/views/page_not_found.blade.php
+++ resources/views/page_not_found.blade.php
@@ -1,20 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="error-template">
-                <h1>
-                    Oops!</h1>
-                <h2>
-                    404 Not Found</h2>
-                <div class="error-details">
-                    Sorry, an error has occured, Requested page not found!
-                </div>
-                <div class="error-actions">
-                    <a href="http://www.jquery2dotnet.com" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
-                        Take Me Home </a><a href="http://www.jquery2dotnet.com" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
-                </div>
-            </div>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Bootstrap Website Templates">
+
+    <title>Bitcoin Trade - one page bootstrap website template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+
+
+</head>
+
+<body>
+
+    <div class="container text-center">
+        <div class="brand">
+            <span class="glyphicon glyphicon-king" aria-hidden="true"></span>
+            <h3 class="text-uppercase">The King Maker</h3>
         </div>
+        <h1 class="head"><span>404</span></h1>
+        <p>Oops! The Page you requested was not found!</p>
+        <a href="#" class="btn-outline"> Back to Home</a>
     </div>
-</div>
+
+   <div class="bwt-footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 copyright">
+            <div class="left-text">Copyright &copy; The King Maker 2018. All Rights Reserved</div>
+        </div>
+      </div>
+    </div>
+   </div>
+   
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Theme JavaScript -->
+    <script src="js/script.js"></script>
+
+</body>
+</html>
