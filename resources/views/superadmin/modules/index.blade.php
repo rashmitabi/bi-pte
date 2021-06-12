51afe54c5ae6fdb6f1dbd5e6f584271eedbc0ad4
rashmitabi <71814733+rashmitabi@users.noreply.github.com>
2021-06-12 13:57:37 +0530

    update

--- resources/views/superadmin/modules/index.blade.php
+++ resources/views/superadmin/modules/index.blade.php
@@ -2,35 +2,32 @@
 @section('content')
 <!-- Page Content  -->
 <div id="content">
-   <section class="top-title-button mb-3">
-      <div class="row mx-0 align-items-center">
-         <div class="col-12 col-md-12 col-xl-8 col-sm-8 left">
-             <h1 class="title mb-4">Manage Module</h1>
-         </div>
-         <div class="col-12 col-md-12 col-xl-4 col-sm-4 right">
-             <button type="button" class="btn btn-primary" onclick="location.href ='{{ route('modules.create') }}'"><i class="fas fa-plus-circle mr-1"></i> New
-                 Module</button>
-         </div>
-      </div>
-   </section>
-   <section class="top-title-button white-bg remove-main-margin mb-3">
-      <div class="row mx-0 align-items-center">
-         <div class="col-12 col-md-12 col-xl-12 col-sm-8 p-0 left">
-            <table id="module" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
-               <thead>
-                  <tr>
-                     <th> <input type="checkbox" class="form-check-input position-relative ml-0" id="exampleCheck1"> </th>
-                     <th>Sr No</th>
-                     <th>Module Name</th>
-                     <th>Module slug</th>
-                     <th>Status</th>
-                     <th>Action</th>
-                  </tr>
-               </thead>
-            </table>
-         </div>
-      </div>
-   </section>
+    <section class="top-title-button mb-3">
+        <div class="row mx-0 align-items-center">
+            <div class="col-12 col-md-12 col-xl-8 col-sm-8 left">
+                <h1 class="title mb-4">Manage Module</h1>
+            </div>
+        </div>
+    </section>
+    <section class="top-title-button white-bg mb-3 remove-main-margin">
+        <div class="row mx-0 align-items-center">
+            <div class="col-12 col-md-12 col-xl-12 col-sm-8 left p-0">
+                <!-- <h1 class="title mb-4">Manage Subscription</h1> -->
+                <table id="managemodule" class="table table-striped table-bordered dt-responsive nowrap common"
+                    style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>SR No</th>
+                            <th>Module Name</th>
+                            <th>Module Slug</th>
+                            <th>Module status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </section>
 </div>
 @endsection
 @section('js-hooks')
