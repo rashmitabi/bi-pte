0cc3124250f489ad51126c9461f24d3fd638fc42
9926830418 <abhishekpatidarap444@gmail.com>
2021-06-21 09:54:58 +0530

    responsive

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -5,9 +5,9 @@
       <div class="dashboard-heading mb-3">
           <h4>Admin DashBoard</h4>
       </div>
-      <div class="row">
-           <div class="col student-col">
-              <div class="common-wrap">
+      <div class="row custom-row">
+           <div class="col-12 col-md-3 col-xl-3 col-sm-3 common-col student-col">
+              <div class="dashboard-common-wrap">
                     <div class="icon">
                       <img src="{{ asset('assets/images/icons/reading.svg') }}">
                    </div>
@@ -20,8 +20,8 @@
                        <h2>81</h2>
                </div> 
            </div>
-           <div class="col mocktest-col">
-               <div class="common-wrap">
+           <div class="col-12 col-md-3 col-xl-3 col-sm-3 common-col mocktest-col">
+               <div class="dashboard-common-wrap">
                     <div class="icon">
                       <img src="{{ asset('assets/images/icons/exam.svg') }}">
                     </div>
@@ -34,8 +34,8 @@
                      <h2>81</h2>
                </div>
             </div>
-            <div class="col practicetest-col">
-                <div class="common-wrap">
+            <div class="col-12 col-md-3 col-xl-3 col-sm-3 common-col practicetest-col">
+                <div class="dashboard-common-wrap">
                     <div class="icon">
                       <img src="{{ asset('assets/images/icons/exam.svg') }}">
                     </div>
@@ -48,8 +48,8 @@
                     <h2>81</h2>
                </div>
             </div>
-            <div class="col institute-col">
-                <div class="common-wrap">
+            <div class="col-12 col-md-3 col-xl-3 col-sm-3  common-col institute-col">
+                <div class="dashboard-common-wrap">
                     <div class="icon">
                       <img src="{{ asset('assets/images/icons/request.svg') }}">
                    </div>
@@ -71,13 +71,13 @@
 
          <!-- tables -->
         <div class="row">
-           <div class="col-6 pl-0">
+           <div class="col-12 col-md-12 col-xl-6 col-sm-12 table-col pl-0 custom-wrap-p">
                <section class="top-title-button white-bg common-wrap mb-3">
                    <div class="col-12 col-md-8 col-xl-8 col-sm-8 left">
                      <h1 class="table-head head-text">Activity Log</h1>
                    </div>
                    <div class="row mx-0 align-items-center">
-                      <div class="col-12 col-md-12 col-xl-12 col-sm-8 left p-0">
+                      <div class="col-12 col-md-12 col-xl-12 col-sm-12 left p-0">
                            <table id="activitylog" class="table  table-bordered dt-responsive nowrap" style="width:100%">
                                <thead>
                                    <tr>
@@ -123,9 +123,9 @@
                     </div>
                 </section>
             </div>
-            <div class="col-6 pl-0">
+            <div class="col-12 col-md-12 col-xl-6 col-sm-12 table-col pl-0 custom-wrap-p">
                 <section class="top-title-button white-bg common-wrap mb-3">
-                   <div class="col-12 col-md-8 col-xl-6 col-sm-7 float-left left">
+                   <div class="col-12 col-md-6 col-xl-6 col-sm-7 float-left left">
                      <h1 class="table-head">Transactions</h1>
                    </div>
                    <div class="col-3 datapicker">
@@ -133,7 +133,7 @@
                           <input type="date" class="form-control" placeholder="To">
                       </div>
                    <div class="row mx-0 align-items-center">
-                      <div class="col-12 col-md-12 col-xl-12 col-sm-8 left p-0">
+                      <div class="col-12 col-md-12 col-xl-12 col-sm-12 left p-0">
                            <table id="transaction" class="table  table-bordered dt-responsive nowrap" style="width:100%">
                                <thead>
                                    <tr>
@@ -186,19 +186,19 @@
                 </section>
             </div>
 
-            <div class="col-6 pl-0">
+            <div class="col-12 col-md-12 col-xl-6 col-sm-12 table-col pl-0 custom-wrap-p">
                <section class="top-title-button white-bg common-wrap mb-3">
-                   <div class="col-12 col-md-8 col-xl-8 col-sm-8 left float-left">
+                   <div class="col-12 col-md-12 col-xl-8 col-sm-8 left float-left">
                      <h4 class="table-head">Institute Subscriptions</h4>
                    </div>
-                   <div class="col-12 col-md-12 col-xl-4 col-sm-2 expired-btn">
+                   <div class="col-12 col-md-12 col-xl-4 col-sm-7 expired-btn">
                        <div class="btn-group" role="group" aria-label="First group">
                            <button type="button" class="btn btn-secondary expired-btn">Expired</button>
                            <button type="button" class="btn btn-secondary near-expired ">Near to Expired</button>
                        </div>
                    </div>
                    <div class="row mx-0 align-items-center">
-                      <div class="col-12 col-md-12 col-xl-12 col-sm-8 left p-0">
+                      <div class="col-12 col-md-12 col-xl-12 col-sm-12 left p-0">
                            <table id="institute" class="table  table-bordered dt-responsive nowrap" style="width:100%">
                                <thead>
                                    <tr>
@@ -256,13 +256,13 @@
                     </div>
                 </section>
             </div>
-            <div class="col-6 pl-0">
+            <div class="col-12 col-md-12 col-xl-6 col-sm-12 table-col pl-0 custom-wrap-p">
                 <section class="top-title-button white-bg common-wrap mb-3">
                    <div class="col-12 col-md-8 col-xl-8 col-sm-7 left">
                      <h1 class="table-head">Top Ranking Institute</h1>
                    </div>
                    <div class="row mx-0 align-items-center">
-                      <div class="col-12 col-md-12 col-xl-12 col-sm-8 left pt-1 p-0">
+                      <div class="col-12 col-md-12 col-xl-12 col-sm-12 left pt-1 p-0">
                            <table id="ranking" class="table  table-bordered dt-responsive nowrap" style="width:100%">
                                <thead>
                                    <tr>
