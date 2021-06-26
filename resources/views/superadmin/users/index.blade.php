8f45dade305fb4001d3e25a55c7a62957796cd1c
rashmitabi <71814733+rashmitabi@users.noreply.github.com>
2021-06-26 12:20:09 +0530

    update

--- resources/views/superadmin/users/index.blade.php
+++ resources/views/superadmin/users/index.blade.php
@@ -5,7 +5,7 @@
   <section class="top-title-button mb-3">
     <div class="row mx-0 align-items-center">
       <div class="col-12 col-md-12 col-xl-8 col-sm-8 left">
-        <h1 class="title mb-4">Manage User</h1>
+        <h1 class="title mb-4">Manage User test</h1>
       </div>
       <div class="col-12 col-md-12 col-xl-4 col-sm-4 right">
         <a href="{{ route('users.create') }}"><button type="button" class="btn btn-primary" ><i class="fas fa-plus-circle mr-1"></i> New
