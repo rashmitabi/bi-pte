2877897b208807725901bd156dfd21922e27d065
rashmitabi <71814733+rashmitabi@users.noreply.github.com>
2021-07-28 15:26:23 +0530

    update

--- resources/views/superadmin/tests/listening/summarize_spoken_item.blade.php
+++ resources/views/superadmin/tests/listening/summarize_spoken_item.blade.php
@@ -136,10 +136,10 @@
                   <div class="form-group mb-2 row">
                     <div class="col-12 p-0">                              
                       <div class="custom-file mb-3" >
-                          <input type="file" class="custom-file-input" onchange="uploadImage()" id="customFile_image789" data-url="{{ route('upload-image') }}" data-token="{{ csrf_token() }}">
+                          <input type="file" class="custom-file-input" onchange="uploadImage()" id="customFile_image789" data-url="{{ route('upload-image') }}" data-token="{{ csrf_token() }}" >
                           <label class="custom-file-label" for="customFile">Select Image</label>
                       </div>
-                      <input type="text" name="upload-image" id="upload-image" class="form-control">
+                      <input type="text" name="upload-image" id="upload-image" class="form-control" placeholder="Here Display the code of Image">
                       <span class="error-msg" id="upload-imageError"></span>
                     </div>
                   </div>
@@ -147,10 +147,10 @@
                   <div class="form-group mb-2 row">
                     <div class="col-12 p-0">                                  
                       <div class="custom-file mb-3">
-                        <input type="file" class="custom-file-input " onchange ="uploadAduio()" id="customFile_audio123" data-url="{{ route('upload-audio') }}" data-token="{{ csrf_token() }}">
+                        <input type="file" class="custom-file-input " onchange ="uploadAduio()" id="customFile_audio123" data-url="{{ route('upload-audio') }}" data-token="{{ csrf_token() }}" >
                         <label class="custom-file-label" for="customFile">Select Audio</label>
                       </div>
-                      <input type="text" name="upload-audio" id="upload-audio" class="form-control" >
+                      <input type="text" name="upload-audio" id="upload-audio" class="form-control" placeholder="Here Display the code of Audio">
                       <span class="error-msg" id="upload-audioError"></span>
                     </div>
                   </div>
