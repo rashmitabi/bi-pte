cf4640035d62f8873e9393e4b251ddad3c96f578
vinod ahuja <vinod.ahuja.bi@gmail.com>
2021-07-29 11:19:34 +0530

    140 solved all
    done

--- app/Http/Middleware/BranchAdmin.php
+++ app/Http/Middleware/BranchAdmin.php
@@ -19,6 +19,9 @@ class BranchAdmin
         if (\Auth::user()->role_id != '2') {
             return redirect('/login');
         }
-        return $next($request);
+        $response = $next($request);
+        return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
     }
 }
